059ac94c7a44a469a12d531ac6b556c5001b59e1
matthewport <matthew@impactbox.co.uk>
2022-10-07 21:50:48 +0100

    got sfdx to respond with json, moved callback function

--- SFDXTools.spoon/init.lua
+++ SFDXTools.spoon/init.lua
@@ -5,7 +5,7 @@ obj.version = "1.0.1"
 obj.author = { name = "Matt Port", email = "mattp0@duck.com" }
 
 obj.SFDXPATH = "/usr/local/bin/sfdx"
-
+json = "--json"
 
 function obj:init()
     w = {}
@@ -13,47 +13,44 @@ function obj:init()
     return setmetatable(w , self )
 end
 
+function showListOutput(exitCode, stdOut, stdErr)
+        -- manipulate string before output
+        print ("exit code " .. exitCode)
+        print("out " .. stdOut)
+        print("err " .. stdErr)
+        alertString = stdOut
+        hs.alert.show(alertString,
+        {
+            strokeWidth  = 2,
+            strokeColor = { white = 1, alpha = 1 },
+            fillColor   = { white = 0, alpha = 0.75 },
+            textColor = { white = 1, alpha = 1 },
+            textFont  = ".AppleSystemUIFont",
+            textSize  = 14,
+            radius = 27,
+            atScreenEdge = 1,
+            fadeInDuration = 0.15,
+            fadeOutDuration = 0.15,
+            padding = nil,
+        },
+        hs.screen.mainScreen(),
+        6  )    
+end
+
 function obj:startDialog()
     hs.focus()
-    button, text = hs.dialog.textPrompt("SFDX", "Enter the alias of the org you'd like to open", "list", "OK", "Cancel", false)
-    print(text)
-    if text == "list" then
-            t = nil
-            t = hs.task.new(
-            -- launch path    
-            this.SFDXPATH, 
-            --callback function
-            function(exitCode, stdOut, stdErr) 
-                -- manipulate string before output
-                alertString = stdOut
-                hs.alert.show(alertString,
-                {
-                    strokeWidth  = 2,
-                    strokeColor = { white = 1, alpha = 1 },
-                    fillColor   = { white = 0, alpha = 0.75 },
-                    textColor = { white = 1, alpha = 1 },
-                    textFont  = ".AppleSystemUIFont",
-                    textSize  = 14,
-                    radius = 27,
-                    atScreenEdge = 1,
-                    fadeInDuration = 0.15,
-                    fadeOutDuration = 0.15,
-                    padding = nil,
-                },
-                hs.screen.mainScreen(),
-                6 
-
-                )    
-                end, 
-                -- streamCallback
-                function(...) return false end,
-                -- args for sfdx
-                {"force:org:list", "--json"})
-
+    button, text = hs.dialog.textPrompt("SFDX", "Enter org to open", "list", "Open", "Cancel", false)
+    if button == 'Open' then
+        if text == 'list' then
+            print(text)
+            print(button)
+                t = nil
+                t = hs.task.new(obj.SFDXPATH, showListOutput, {'force:org:list', '--json'})
                 t:start()
-                
-    else
-        hs.execute("sfdx force:org:open -u" .. text, true)
+                    
+        else
+            hs.execute("sfdx force:org:open -u" .. text, true)
+        end
     end
 end
 
@@ -61,9 +58,11 @@ function obj:bindHotKeys(mapping)
     spec = mapping[1]
     key = mapping[2]
     if hs.hotkey.assignable(spec,key) == true then
-        hs.hotkey.bind(spec, key, obj:startDialog())
-    else 
+        hs.hotkey.bind(spec, key, function()
+            obj:startDialog()
+        end)
+        else 
         print("Could not bind keys, try a different combination")
     end
-end
+end 
 return obj
